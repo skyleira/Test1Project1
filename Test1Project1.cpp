88dbf13179b1cf3a9e8c2627c2728c07925acfd2
skyleira <tyler.b.hively@gmail.com>
2024-10-16 06:49:16 -0400

    Revised program to work correctly.

--- Test1Project1.cpp
+++ Test1Project1.cpp
@@ -11,42 +11,63 @@ using namespace std;
 int main()
 {
     double totalSales;
-    double commissionRate;
     double payAdvance;
+    double commissionRate;
     double totalCommission;
     double takehomePay;
     
     cout << "-------Pay Result Calculator-------" << endl;
     cout << "Please input total sales amount for this month: " << endl;
     cin >> totalSales; 
-        if (totalSales < 10000)
-            commissionRate = .05;
+
+    if (totalSales < 10000)
+    {
+     commissionRate = .05;
+    }
+     else 
+     
         if (totalSales < 14999)
-            commissionRate = .10;
-        if (totalSales < 17999)
-            commissionRate = .12;
-        if (totalSales < 21999)
-            commissionRate = .14;
-        if (totalSales > 22000)
-            commissionRate = .16;
-    
-    
-    totalCommission = totalSales * commissionRate;
+        {
+          commissionRate = .10;
+        }
+        else
+        
 
-    cout << "Please input total pay advance amount for this month: " << endl;
-    cin >> payAdvance;
+            if (totalSales < 17999)
+            {
+               commissionRate = .12;
+            }
+            else
 
-    takehomePay = totalCommission - payAdvance;
+                if (totalSales < 21999)
+                {
+                   commissionRate = .14;
+                }
+                else
+                    {
+                      commissionRate = .16;
+                    }
 
-    cout << endl;
-    cout << "[ Pay Results ]" << endl;
-    cout << "---------------" << endl;
-    cout << "Total sales for this month: " << totalSales << endl;
-    cout << "Commission Rate: " << commissionRate << endl;
-    cout << "Commission Earned: " << totalCommission << endl;
-    cout << "Advanced Pay: " << payAdvance << endl;
+                      totalCommission = (totalSales * commissionRate);
+                      cout << "Please input total pay advance amount for this month: " << endl;
+                      cin >> payAdvance;
 
+                      takehomePay = totalCommission - payAdvance;
 
+                      cout << endl;
+                      cout << "[ Pay Results ]" << endl;
+                      cout << "---------------" << endl;
+                      cout << "Total sales for this month: $" << totalSales << endl;
+                      cout << "Commission Rate: $" << commissionRate << endl;
+                      cout << "Commission Earned: $" << totalCommission << endl;
+                      cout << "Advanced Pay: $" << payAdvance << endl;
+                      
+                      if (takehomePay < 0)
+                      {
+                          cout << "You Owe: $" << takehomePay << endl;
+                      }
+                      else
+                          cout << "Total Earnings: $" << takehomePay << endl;
 
 
 
